0d58af55cc676f2f2511822c1d0709df0b432628
webbydevvy <charlie@civo.com>
2019-06-14 08:57:28 +0100

    Implement counter onClick functionality, to be developed into API callback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect in use");
+  });
+
   return(
     <div className="App">
-      <h1>Hello, React!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
